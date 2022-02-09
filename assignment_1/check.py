0a2d281e5ada75f47151dcf9c247e81211aa3182
DontamsettiHemanth <dontamsettihemanth@gmail.com>
2022-02-09 05:41:02 +0000

    logwarn logerr check.py

--- assignment_1/check.py
+++ assignment_1/check.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
 import rospy
 #from helper import *
-
-rospy.logwarn("Hit")#,True,0.512,str(point(1,1)))
-rospy.sleep(1)
+if __name__ == "__main__":
+    rospy.init_node("check")
+    rospy.logwarn("Hit")#,True,0.512,str(point(1,1)))
+    rospy.sleep(1)
+    rospy.logerr("Hit")
+    rospy.logwarn("Hit")
+    rospy.spin()
