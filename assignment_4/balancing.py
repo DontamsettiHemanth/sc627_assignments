bcbe3a69237cb8afc7f6c9c617fb5a29f16f42de
DontamsettiHemanth <dontamsettihemanth@gmail.com>
2022-04-04 12:45:54 +0000

    Asgn4 try2: corrected subscribers, prob: bot_2 process dies

--- assignment_4/balancing.py
+++ assignment_4/balancing.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
-from sc627_helper.msg import ObsData
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 import math
@@ -9,6 +8,8 @@ import numpy as np
 
 ANG_MAX = math.pi/18
 VEL_MAX = 0.15
+rospy.init_node('assign4', anonymous=True)
+
 
 def velocity_convert(x, y, theta, vel_x, vel_y):
     '''
@@ -16,15 +17,15 @@ def velocity_convert(x, y, theta, vel_x, vel_y):
     Velocity vector (vel_x, vel_y)
     '''
 
-    gain_ang = 1 #modify if necessary
-    
+    gain_ang = 1  # modify if necessary
+
     ang = math.atan2(vel_y, vel_x)
     if ang < 0:
         ang += 2 * math.pi
-    
+
     ang_err = min(max(ang - theta, -ANG_MAX), ANG_MAX)
 
-    v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), -VEL_MAX), VEL_MAX)
+    v_lin = min(max(math.cos(ang_err) * math.sqrt(vel_x**2 + vel_y**2), -VEL_MAX), VEL_MAX)
     v_ang = gain_ang * ang_err
     return v_lin, v_ang
 
@@ -37,11 +38,11 @@ class Decentralised_bot:
         self.lbot = None
         self.rbot = None
         self.bot = None
-        self.bot_sub = rospy.Subscriber('/odom', Odometry, callback_odom) #topic name fixed
-        self.lbot_sub = rospy.Subscriber('/left_odom', Odometry, callback_left_odom) #topic name fixed
-        self.rbot_sub = rospy.Subscriber('/right_odom', Odometry, callback_left_odom) #topic name fixed
+        self.bot_sub = rospy.Subscriber('/odom', Odometry, self.callback_odom)
+        self.lbot_sub = rospy.Subscriber('/left_odom', Odometry, self.callback_left_odom)
+        self.rbot_sub = rospy.Subscriber('/right_odom', Odometry, self.callback_right_odom)
 
-        self.pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        self.pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     def callback_odom(self, data):
         '''
@@ -52,7 +53,6 @@ class Decentralised_bot:
         v = np.array([data.twist.twist.linear.x, data.twist.twist.linear.y])
         self.bot = {'x': x, 'th': theta, 'v': v, 'w': data.twist.twist.angular.z}
         self.t = data.header.stamp.secs + data.header.stamp.nsecs*1e-9
-        pass
 
     def callback_left_odom(self, data):
         '''
@@ -61,7 +61,6 @@ class Decentralised_bot:
         theta = 2*np.arctan2(data.pose.pose.orientation.z, data.pose.pose.orientation.w)
         x = np.array([data.pose.pose.position.x, data.pose.pose.position.y])
         self.lbot = {'x': x, 'th': theta}
-        pass
 
     def callback_right_odom(self, data):
         '''
@@ -70,12 +69,10 @@ class Decentralised_bot:
         theta = 2*np.arctan2(data.pose.pose.orientation.z, data.pose.pose.orientation.w)
         x = np.array([data.pose.pose.position.x, data.pose.pose.position.y])
         self.rbot = {'x': x, 'th': theta}
-        pass
-    
+
     def velocity_convert(self, vel):
         '''
-        Robot pose (x, y, theta)  Note - theta in (0, 2pi)
-        Velocity vector (vel_x, vel_y)
+        Velocity vector vel = (vel_x, vel_y)
         '''
         theta = self.bot['th']
         vel_x, vel_y = vel
@@ -86,7 +83,7 @@ class Decentralised_bot:
 
         ang_err = min(max(ang - theta, -ANG_MAX), ANG_MAX)
 
-        v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), -VEL_MAX), VEL_MAX)
+        v_lin = min(max(math.cos(ang_err) * math.sqrt(vel_x**2 + vel_y**2), -VEL_MAX), VEL_MAX)
         v_ang = gain_ang * ang_err
         return v_lin, v_ang
 
@@ -95,45 +92,46 @@ class Decentralised_bot:
         Move to midpoint of l and r bots
         TODO Take care of collision
         '''
+        if (self.lbot is None) or (self.rbot is None) or (self.bot is None):
+            rospy.loginfo("self.bot/rbot/lbot is None")
+            return
         mid = 0.5*(self.lbot['x'] + self.rbot['x'])
         PO = mid - self.bot['x']
         dist = np.sqrt(sum(PO*PO))
         if dist < self.tolerance:
             self.num += 1
-            #publishing zero velocities
+            # publishing zero velocities
             vel_msg = Twist()
             self.pub_vel.publish(vel_msg)
             return
-        self.num = 0   
-        vel = self.velocity_convert(PO)
-        #publishing the velocities
+        self.num = 0
+        v_lin, v_ang = self.velocity_convert(PO)
+        # publishing the velocities
         vel_msg = Twist()
         vel_msg.linear.x = v_lin
         vel_msg.angular.z = v_ang
         self.pub_vel.publish(vel_msg)
 
 
-rospy.init_node('assign4', anonymous = True)
-
 r = rospy.Rate(30)
-bot_i = Decentralised_bot(0)
+bot_i = Decentralised_bot()
 path = []
 i = 0
-while i < 1e7:
+while i < 1e5:
     i += 1
 
 vel_msg = Twist()
 bot_i.pub_vel.publish(vel_msg)
+iter = 0
+while (~rospy.is_shutdown()) and (bot_i.num < 10):
+    if bot_i.bot is not None:
+        bot_i.consensus()
+        # store robot path with time stamps (data available in odom topic)
+        path.append([bot_i.t, bot_i.bot['x'][0], bot_i.bot['x'][1]])
 
-while (~rospy.is_shutdown()) and (bot_i.num < 10): #replace with balancing reached?
-    
-    bot_i.concensus()
-    #store robot path with time stamps (data available in odom topic)
-    path.append([bot_i.t, bot_i.bot['x'][0], bot_i.bot['x'][1]])
-    
-    iter += 1
-    if iter % 100 == 0:
-        rospy.loginfo("Completed %r iterations", iter)
+        iter += 1
+        if iter % 1000 == 0:
+            rospy.loginfo("Completed %r iterations", iter)
     r.sleep()
 
 if (~rospy.is_shutdown()):
@@ -148,6 +146,3 @@ with open("/root/catkin_ws/src/sc627_assignments/assignment_4/output.txt", "a") 
         pass
     file.write("\n")
 exit(1)
-
-
-
