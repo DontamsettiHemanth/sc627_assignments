e4723a4e1f3c2741dfa639d24f3cb000999c6b5e
DontamsettiHemanth <dontamsettihemanth@gmail.com>
2022-04-06 02:23:25 +0000

    Asgn4 try5: Reached goal but not exiting the loop

--- assignment_3/collision_avoidance.py
+++ assignment_3/collision_avoidance.py
@@ -13,6 +13,14 @@ VEL_MAX = 0.15
 rospy.init_node('assign3', anonymous=True)
 
 
+def angle_loop(th):
+    while th < -np.pi:
+        th += 2*np.pi
+    while th > np.pi:
+        th -= 2*np.pi
+    return th
+
+
 class velocity_obstacles:
 
     def __init__(self):
@@ -20,11 +28,10 @@ class velocity_obstacles:
         self.goal = np.array([5, 0])
         self.obs_xv = None
         self.bot_xv = None
-        self.obs_sub = rospy.Subscriber('/obs_data', ObsData, self.callback_obs)  # topic name fixed
-        self.bot_sub = rospy.Subscriber('/bot_1/odom', Odometry, self.callback_odom)  # topic name fixed
+        self.obs_sub = rospy.Subscriber('/obs_data', ObsData, self.callback_obs)
+        self.bot_sub = rospy.Subscriber('/bot_1/odom', Odometry, self.callback_odom)
 
-        self.pub_vel = rospy.Publisher('/bot_1/cmd_vel', Twist, queue_size = 10)
-        # rospy.sleep(.3)
+        self.pub_vel = rospy.Publisher('/bot_1/cmd_vel', Twist, queue_size=10)
 
     def velocity_convert2(self, x, y, theta, vel_x, vel_y):
         '''
@@ -43,7 +50,7 @@ class velocity_obstacles:
 
         ang_err = min(max(ang - theta, -ANG_MAX), ANG_MAX)
 
-        v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), -VEL_MAX), VEL_MAX)
+        v_lin = min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), -VEL_MAX), VEL_MAX)
         v_ang = gain_ang * ang_err
         return v_lin, v_ang
 
@@ -89,14 +96,14 @@ class velocity_obstacles:
         pass
 
     def max_v(self, v1, v2):
-        PG = self.bot_xv['x'] - self.goal
+        PG = -self.bot_xv['x'] + self.goal
         th_g = np.arctan2(PG[1], PG[0])
-        alpha = 30*np.pi/180
-        th_1 = v1[1] + self.bot_xv['th'] - th_g
-        th_2 = v2[1] + self.bot_xv['th'] - th_g
+        alpha = 45*np.pi/180
+        th_1 = angle_loop(v1[1] + self.bot_xv['th'] - th_g)
+        th_2 = angle_loop(v2[1] + self.bot_xv['th'] - th_g)
         if (-alpha <= th_1) and (th_1 <= alpha):
             if (-alpha <= th_2) and (th_2 <= alpha):
-                
+
                 if v2[0] >= v1[0]+0.01:
                     return v2
                 elif v1[0] >= v2[0]+0.01:
@@ -115,49 +122,49 @@ class velocity_obstacles:
             return np.array([0, 0])
 
 
-R = 0.15
+R = 2*0.15
 r = rospy.Rate(30)
 v_search = [i*0.01 for i in range(int(100*VEL_MAX), 0, -1)]
+v_search.append(0)
 th_search = [i*ANG_MAX/10 for i in range(-10, 11)]
 VO = velocity_obstacles()
 path = []
-i = 0
-while i < 1e7:
-    i += 1
-# rospy.sleep(3.)
+while (VO.obs_xv is None) or (VO.bot_xv is None):
+    pass
 vel_msg = Twist()
 VO.pub_vel.publish(vel_msg)
 
 iter = 0
-while (~rospy.is_shutdown()) and sum((VO.bot_xv['x'] - VO.goal)*(VO.bot_xv['x'] - VO.goal)) > 0.05:  # replace with destination reached?
-    # calculate collision cone below
-    once_free = False  # completed one search of all angles without collision
+Goal_rel = VO.goal - VO.bot_xv['x']
+while (~rospy.is_shutdown()) or (sum(Goal_rel*Goal_rel) > 0.1**2):
+    once_free = False  # true when a v and th are found without collision with all obs
     free_point = np.array([0, 0])
-    iter += 1
-    if iter % 100 == 0:
-        rospy.loginfo("Completed %r iterations", iter)
     # rospy.loginfo("Completed %r iterations", iter)
     for v in v_search:
         for th in th_search:
             free_obs = True  # velocity vector avoids all obstacles?
+            bot_th = th + VO.bot_xv['th']
+            bot_v = np.array([v*np.cos(bot_th), v*np.sin(bot_th)])
             for obs in VO.obs_xv:
-                # MV strategy
-                bot_w = th - VO.bot_xv['th']
-                bot_th = th + VO.bot_xv['th']
-                bot_v = np.array([v*np.cos(bot_th), v*np.sin(bot_th)])
+                # collision Checking
                 v_rel = bot_v - obs['v']
-                PO = VO.bot_xv['x'] - obs['x']
+                PO = -VO.bot_xv['x'] + obs['x']
                 th_cri = np.arcsin(R/np.sqrt(sum(PO*PO)))
                 alpha = np.arccos(sum(v_rel*PO)/np.sqrt(sum(v_rel*v_rel)*sum(PO*PO)))
+
                 if alpha < th_cri:
-                    # collides this obstacle
-                    free_obs = False
-                    break
+                    # possibility to collide this obstacle
+                    t_d = 15
+                    # check if |v|^2>|PO|^2/t_d^2 for collision
+                    if sum(v_rel*v_rel)*(t_d**2) >= np.sum(PO*PO):
+                        free_obs = False
+                        break
             if free_obs:
-                once_free = True
                 free_point = VO.max_v(free_point, np.array([v, th]))
-                if iter % 100 == 0:
-                    print free_point
+                if sum(free_point == np.array([0, 0])) != 2:
+                    once_free = True
+                    if iter % 100 == 0:
+                        print free_point
             pass
         if once_free:
             break
@@ -172,6 +179,9 @@ while (~rospy.is_shutdown()) and sum((VO.bot_xv['x'] - VO.goal)*(VO.bot_xv['x'] 
 
     # storing robot path with time stamps (data available in odom topic)
     path.append([VO.t, VO.bot_xv['x'][0], VO.bot_xv['x'][1]])
+    iter += 1
+    if iter % 100 == 0:
+        rospy.loginfo("Completed %r iterations", iter)
     r.sleep()
 
 vel_msg = Twist()
